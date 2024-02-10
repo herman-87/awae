180c9cffd343cd4a58d5e839af6b81f32c4037ec
HangaWilfried <wilfriedhanga5@gmail.com>
2024-02-10 13:51:38 +0100

    implement login on public part

---
+++ src/components/TextField.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col gap-0.5 lg:gap-2 text-gray-900">
+    <label :for="label">{{ label }}</label>
+    <input
+      class="rounded lg:rounded-lg p-2 lg:py-4 px-2 outline-none border border-gray-300 focus:border-none focus:outline-blue-400"
+      type="text"
+      v-model="model"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const model = defineModel()
+
+defineProps<{
+  label: string
+  placeholder: string
+}>()
+</script>
---
+++ src/components/TwButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button :class="['font-bold flex justify-center items-center rounded lg:rounded-lg', theme.toLowerCase()]">
+    <slot name="content" :cta="cta">{{ cta }}</slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  cta: string;
+  theme: string;
+}>()
+</script>
+
+<style scoped>
+.blue {
+  @apply bg-blue-500 text-white p-2 lg:px-2 lg:py-4
+}
+</style>
